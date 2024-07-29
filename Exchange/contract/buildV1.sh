9b7f81062ef8b2b31751c1666ffa44819108748b
VoltexKyle <kyle@vortexgaming.io>
2024-07-29 14:21:07 +0900

    delete

--- Exchange/contract/buildV1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo ">> Building contractV1"
-
-near-sdk-js build src/contractV1.ts build/exchange.wasm
-
-mv "build" "buildV1"
--- Exchange/contract/buildV2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo ">> Building contractV2"
-
-near-sdk-js build src/contractV2.ts build/exchange.wasm
-
-rm -rf buildV2
-
-mv "build" "buildV2"
